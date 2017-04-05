32d42de44ffc119d0ca51e3f40da1371b85345a9
Samuel Younkin <samuel.younkin@gmail.com>
2017-04-05 11:28:34 -0500

    Update Tomek's roadTest.R

--- tomek/roadTest.R
+++ tomek/roadTest.R
@@ -1,29 +1,49 @@
 rm(list=ls())
+
 library("ITHIM") # Make sure you're on the devel branch
 
-ITHIM <- createITHIM()
+# SafetyInNumbers and DistByRoadType Baseline data already included via default input files
 
-# DistByRoadType
+ITHIM.baseline <- createITHIM()
 
-ITHIM.baseline <- readDistByRoadType(ITHIM, system.file("distByRoadTypeBaseline.csv", package = "ITHIM"))
-ITHIM.scenario <- readDistByRoadType(ITHIM, system.file("distByRoadTypeScenario.csv", package = "ITHIM"))
+# Scenario DistByRoadType
 
-# SafetyInNumbers
-
-ITHIM.baseline <- readSafetyInNumbers(ITHIM.baseline, system.file("SiN.csv", package = "ITHIM"))
-ITHIM.scenario <- readSafetyInNumbers(ITHIM.scenario, system.file("SiN.csv", package = "ITHIM"))
+ITHIM.scenario <- update(ITHIM.baseline, list(distRoadType = readDistByRoadType(system.file("distByRoadTypeScenario.csv", package = "ITHIM"))))
 
 # RoadInjuries for baseline
 
-ITHIM.baseline <- readRoadInjuries(ITHIM.baseline, system.file("roadInjuriesBaseline.csv", package = "ITHIM"))
+ITHIM.baseline <- update(ITHIM.baseline, list(roadInjuries = readRoadInjuries(system.file("roadInjuriesBaseline.csv", package = "ITHIM"))))
 
 # RoadInjuries for scenario using scenario multiplier and baseline RoadInjuries
 
 ITHIM.scenario <- updateRoadInjuries(ITHIM.baseline, ITHIM.scenario)
 
-##################################################################
-# refactored functions - individual call
+## rm(list=ls())
+## library("ITHIM") # Make sure you're on the devel branch
 
-testComputeMultiplier <- computeMultiplier(baseline = ITHIM.baseline, scenario = ITHIM.scenario, safetyInNumbers = getSiN(ITHIM.baseline))
+## ITHIM <- createITHIM()
 
-testMultiplyInjuries <- multiplyInjuries(ITHIM.baseline = ITHIM.baseline, ITHIM.scenario = ITHIM.scenario)
+## # DistByRoadType
+
+## ITHIM.baseline <- readDistByRoadType(ITHIM, system.file("distByRoadTypeBaseline.csv", package = "ITHIM"))
+## ITHIM.scenario <- readDistByRoadType(ITHIM, system.file("distByRoadTypeScenario.csv", package = "ITHIM"))
+
+## # SafetyInNumbers
+
+## ITHIM.baseline <- readSafetyInNumbers(ITHIM.baseline, system.file("SiN.csv", package = "ITHIM"))
+## ITHIM.scenario <- readSafetyInNumbers(ITHIM.scenario, system.file("SiN.csv", package = "ITHIM"))
+
+## # RoadInjuries for baseline
+
+## ITHIM.baseline <- readRoadInjuries(ITHIM.baseline, system.file("roadInjuriesBaseline.csv", package = "ITHIM"))
+
+## # RoadInjuries for scenario using scenario multiplier and baseline RoadInjuries
+
+## ITHIM.scenario <- updateRoadInjuries(ITHIM.baseline, ITHIM.scenario)
+
+## ##################################################################
+## # refactored functions - individual call
+
+## testComputeMultiplier <- computeMultiplier(baseline = ITHIM.baseline, scenario = ITHIM.scenario, safetyInNumbers = getSiN(ITHIM.baseline))
+
+## testMultiplyInjuries <- multiplyInjuries(ITHIM.baseline = ITHIM.baseline, ITHIM.scenario = ITHIM.scenario)
