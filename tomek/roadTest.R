9bb5abfc840d75045c1367ccd66d058bf1786c56
Samuel Younkin <samuel.younkin@gmail.com>
2017-04-25 10:53:38 -0500

    Fixed a few bugs
    Most of the problem is with external data.  Need to improve that.

--- tomek/roadTest.R
+++ tomek/roadTest.R
@@ -8,15 +8,15 @@ ITHIM.baseline <- createITHIM()
 
 # Scenario DistByRoadType
 
-ITHIM.scenario <- update(ITHIM.baseline, list(distRoadType = readDistByRoadType(system.file("distByRoadTypeScenario.csv", package = "ITHIM"))))
+ITHIM.scenario <- update(ITHIM.baseline, list(distRoadType = ITHIM:::readDistByRoadType(system.file("distByRoadTypeScenario.csv", package = "ITHIM"))))
 
 # RoadInjuries for baseline
 
-ITHIM.baseline <- update(ITHIM.baseline, list(roadInjuries = readRoadInjuries(system.file("roadInjuriesBaseline.csv", package = "ITHIM"))))
+ITHIM.baseline <- update(ITHIM.baseline, list(roadInjuries = ITHIM:::readRoadInjuries(system.file("roadInjuries.csv", package = "ITHIM"))))
 
 # RoadInjuries for scenario using scenario multiplier and baseline RoadInjuries
 
-ITHIM.scenario <- updateRoadInjuries(ITHIM.baseline, ITHIM.scenario)
+ITHIM.scenario <- ITHIM:::updateRoadInjuries(ITHIM.baseline, ITHIM.scenario)
 
 ## rm(list=ls())
 ## library("ITHIM") # Make sure you're on the devel branch
