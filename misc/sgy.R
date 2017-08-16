1824f3cf3a64f94ae101c69e0536db5b73cd1d90
Samuel Younkin <samuel.younkin@gmail.com>
2017-08-16 18:09:26 -0500

    Deleted sgy.R and created methodsPaper.tex

--- misc/sgy.R
+++
@@ -1,4 +0,0 @@
-DaneTravelMeansByTime <- getCountyMeansTravelByTime(state = 55,county = 025)
-DaneTravelMeansByTime <- within(DaneTravelMeansByTime, travelTime <- factor(travelTime, levels = c("minLT10", "min10to14", "min15to19","min20to24","min25to29","min30to34","min35to44","min45to59","minOver60")))
-DaneTravelMeansByTime <- within(DaneTravelMeansByTime, mode <- factor(mode, levels = c("drive","walk","bicycle","transit")))
-ggplot(DaneTravelMeansByTime, aes(x = travelTime, y = estimate, fill = mode)) + geom_bar(stat = "identity", position = "dodge") + theme_bw() + ylab("Number of Commuters") + xlab("Duration of Commute")
